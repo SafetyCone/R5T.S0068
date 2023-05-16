2f515415ba1d49a1cf29d4ac43724e623a116199
davidcoats <david@getsafetycone.com>
2023-05-16 14:00:27 -0700

    Initial commit.

---
+++ source/R5T.S0068/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0068
+{
+	/// <summary>
+	/// Code element descriptor conversion convention scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
